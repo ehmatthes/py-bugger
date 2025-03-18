b5cd224f47c3c7717076bd2241ff0d582ace615e
Eric Matthes <ehmatthes@gmail.com>
2025-03-18 10:25:20 -0400

    Fixtures used in all test functions.

--- tests/e2e_tests/test_basic_behavior.py
+++ tests/e2e_tests/test_basic_behavior.py
@@ -17,25 +17,29 @@ import pytest
 # --- Fixtures ---
 
 @pytest.fixture(scope="session")
-def python_cmd():
-    """Return path to Python executable."""
-    path_root = Path(__file__).parents[2]
-    return path_root / ".venv" / "bin" / "python"
-
-@pytest.fixture(scope="session")
 def e2e_config():
     """Resources useful to most tests."""
     class Config:
+        # Paths
         path_root = Path(__file__).parents[2]
-        path_sample_code = path_root / "tests"/ "sample_code" / "sample_scripts"
-        path_name_picker = path_sample_code / "name_picker.py"
+
+        path_tests = path_root / "tests"
+
+        path_reference_files = path_tests / "e2e_tests" / "reference_files"
+
+        path_sample_code = path_tests / "sample_code"
+        path_sample_scripts = path_sample_code / "sample_scripts"
+        path_name_picker = path_sample_scripts / "name_picker.py"
+
+        # Python executable
+        python_cmd = path_root / ".venv" / "bin" / "python"
 
     return Config()
 
 
 # --- Test functions ---
 
-def test_bare_call(tmp_path_factory, python_cmd, e2e_config):
+def test_bare_call(tmp_path_factory, e2e_config):
     """Test that bare py-bugger call does not modify file."""
 
     # Copy sample code to tmp dir.
@@ -49,48 +53,37 @@ def test_bare_call(tmp_path_factory, python_cmd, e2e_config):
     cmd = f"py-bugger"
     cmd_parts = shlex.split(cmd)
     stdout = subprocess.run(cmd_parts, capture_output=True).stdout.decode()
-    # msg = "A bare py-bugger call makes no changes to your project.\nYou must be explicit about what kinds of errors you want to induce in the project."
-    # assert msg in stdout
-    path_reference_files = e2e_config.path_root / "tests" / "e2e_tests" / "reference_files"
-    path_bare_output = path_reference_files / "bare.txt"
+
+    # Verify output.
+    path_bare_output = e2e_config.path_reference_files / "bare.txt"
     assert stdout == path_bare_output.read_text()
 
-
-    # Check that file is unchanged.
+    # Check that .py file is unchanged.
     assert filecmp.cmp(e2e_config.path_name_picker, path_dst)
 
 
-def test_help(python_cmd):
+def test_help(e2e_config):
     """Test output of `py-bugger --help`."""
-    path_root = Path(__file__).parents[2]
     cmd = "py-bugger --help"
     cmd_parts = shlex.split(cmd)
     stdout = subprocess.run(cmd_parts, capture_output=True).stdout.decode()
 
-    path_reference_files = path_root / "tests" / "e2e_tests" / "reference_files"
-    path_help_output = path_reference_files / "help.txt"
-    help_txt = path_help_output.read_text()
-
-    assert stdout == help_txt
+    path_help_output = e2e_config.path_reference_files / "help.txt"
+    assert stdout == path_help_output.read_text()
 
 
-def test_modulenotfounderror(tmp_path_factory, python_cmd):
+def test_modulenotfounderror(tmp_path_factory, e2e_config):
     """py-bugger --exception-type ModuleNotFoundError"""
 
     # Copy sample code to tmp dir.
     tmp_path = tmp_path_factory.mktemp("sample_code")
     print(f"\nCopying code to: {tmp_path.as_posix()}")
 
-    path_root = Path(__file__).parents[2]
-    path_sample_code = path_root / "tests"/ "sample_code" / "sample_scripts"
-    path_name_picker = path_sample_code / "name_picker.py"
-    assert path_name_picker.exists()
-
-    path_dst = tmp_path / path_name_picker.name
-    shutil.copyfile(path_name_picker, path_dst)
+    path_dst = tmp_path / e2e_config.path_name_picker.name
+    shutil.copyfile(e2e_config.path_name_picker, path_dst)
 
     # Run file, should raise no issues.
-    cmd = f"{python_cmd} {path_dst.as_posix()}"
+    cmd = f"{e2e_config.python_cmd} {path_dst.as_posix()}"
     cmd_parts = shlex.split(cmd)
     subprocess.run(cmd_parts, check=True)
 
@@ -100,7 +93,7 @@ def test_modulenotfounderror(tmp_path_factory, python_cmd):
     subprocess.run(cmd_parts)
 
     # Run file again, should raise ModuleNotFoundError.
-    cmd = f"{python_cmd} {path_dst.as_posix()}"
+    cmd = f"{e2e_config.python_cmd} {path_dst.as_posix()}"
     cmd_parts = shlex.split(cmd)
     stderr = subprocess.run(cmd_parts, capture_output=True).stderr.decode()
     assert "Traceback (most recent call last)" in stderr
