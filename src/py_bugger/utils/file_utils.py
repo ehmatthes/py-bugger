69e46e6d3b7cc92d6799ef5172fc21cd7e5b4846
Eric Matthes <ehmatthes@gmail.com>
2025-03-24 20:50:55 -0400

    Current tests pass with new util function.

--- src/py_bugger/utils/file_utils.py
+++ src/py_bugger/utils/file_utils.py
@@ -11,28 +11,23 @@ def get_py_files(target_dir):
     path = target_dir / ".git"
     using_git = path.exists()
 
-    # if using_git:
-    #     cmd = 'git ls-files "*.py"'
-    #     cmd_parts = shlex.split(cmd)
-    #     py_files = subprocess.run(cmd_parts, capture_output=True).stdout.decode().strip().splitlines()
+    if using_git:
+        cmd = 'git ls-files "*.py"'
+        cmd_parts = shlex.split(cmd)
+        py_files = subprocess.run(cmd_parts, capture_output=True).stdout.decode().strip().splitlines()
 
-    #     # Don't modify any test-related files.
-    #     py_files = [Path(f) for f in py_files]
-    #     py_files = [pf for pf in py_files if "tests/" not in pf.as_posix()]
+        # Don't modify any test-related files.
+        py_files = [Path(f) for f in py_files]
+        py_files = [pf for pf in py_files if "tests/" not in pf.as_posix()]
 
-    #     return py_files
+        return py_files
 
     # Project does not seem to be using Git. Return all .py files not in .venv, and
     # outside of any tests/ dir, build/ dir, or dist/ dir.
-
     py_files = target_dir.rglob("*.py")
     py_files = [pf for pf in py_files if ".venv/" not in pf.as_posix()]
     py_files = [pf for pf in py_files if "tests/" not in pf.as_posix()]
     py_files = [pf for pf in py_files if "build/" not in pf.as_posix()]
     py_files = [pf for pf in py_files if "dist/" not in pf.as_posix()]
-    breakpoint()
-
-
-    sys.exit()
 
     return py_files
