3e5a5d1ee4c2910d644e3973af08b93aad9ac260
Eric Matthes <ehmatthes@gmail.com>
2025-03-25 10:10:30 -0400

    No need to track kinds of bugs at this point.

--- src/py_bugger/py_bugger.py
+++ src/py_bugger/py_bugger.py
@@ -14,14 +14,9 @@ def main(exception_type, target_dir, num_bugs):
     # Get a list of .py files we can consider modifying.
     py_files = file_utils.get_py_files(target_dir)
 
-    # Track bugs that are introduced. For now, just a list of exceptions
-    # that are induced. May expand to a sequence of dataclass objects with
-    # more info about each bug?
-    bugs_introduced = []
-
     # Currently, just one exception type. When multiple are supported, implement
     # more complex logic for choosing which ones to introduce, and tracking bugs.
     if exception_type == "ModuleNotFoundError":
-        new_bugs_made = buggers.module_not_found_bugger(py_files[:], num_bugs)
+        new_bugs_made = buggers.module_not_found_bugger(py_files, num_bugs)
 
         # DEV: Logic here about success or failure.
