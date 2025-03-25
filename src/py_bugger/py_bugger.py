7cb23c0a86143ec739ed715a4b57231ca55d8fcb
Eric Matthes <ehmatthes@gmail.com>
2025-03-24 22:28:28 -0400

    Picks .py file to modify randomly.

--- src/py_bugger/py_bugger.py
+++ src/py_bugger/py_bugger.py
@@ -20,8 +20,9 @@ class ImportCollector(cst.CSTVisitor):
 class ImportModifier(cst.CSTTransformer):
     """Modify imports in the user's project."""
 
-    def __init__(self, nodes_to_break):
+    def __init__(self, nodes_to_break, bugs_introduced):
         self.nodes_to_break = nodes_to_break
+        self.bugs_introduced = bugs_introduced
 
     def leave_Import(self, original_node, updated_node):
         """Modify a direct `import <package>` statement."""
@@ -39,6 +40,9 @@ class ImportModifier(cst.CSTTransformer):
             # Modify the node name.
             new_names = [cst.ImportAlias(name=cst.Name(new_name))]
 
+            # Update number of bugs.
+            self.bugs_introduced.append("ModuleNotFoundError")
+
             return updated_node.with_changes(names=new_names)
 
         return updated_node
@@ -50,33 +54,44 @@ def main(exception_type, target_dir, num_bugs):
     if seed := os.environ.get("PY_BUGGER_RANDOM_SEED"):
         random.seed(int(seed))
 
+    if target_dir:
+        target_dir = Path(target_dir)
+    else:
+        target_dir = Path(os.getcwd())
+    py_files = file_utils.get_py_files(target_dir)
+
+    # Track bugs that are introduced.
+    bugs_introduced = []
+
     if exception_type == "ModuleNotFoundError":
-        print("Introducing a ModuleNotFoundError...")
 
-        # Get the first .py file in the project's root dir.
-        if target_dir:
-            target_dir = Path(target_dir)
-            assert target_dir.exists()
-        else:
-            target_dir = Path(os.getcwd())
+        # Keep trying .py files until we make a bug.
+        bug_made = False
+        py_files_copy = py_files[:]
+        random.shuffle(py_files_copy)
 
-        py_files = file_utils.get_py_files(target_dir)
-        path = py_files[0]
+        while py_files and (not bug_made):
+            # Parse .py file.
+            path = py_files_copy.pop()
+            source = path.read_text()
+            tree = cst.parse_module(source)
 
-        # Read user's code.
-        source = path.read_text()
-        tree = cst.parse_module(source)
+            # Collect all import nodes.
+            import_collector = ImportCollector()
+            tree.visit(import_collector)
 
-        # Collect all import nodes.
-        import_collector = ImportCollector()
-        tree.visit(import_collector)
+            if not import_collector.import_nodes:
+                continue
 
-        nodes_to_break = random.choices(import_collector.import_nodes, k=num_bugs)
+            nodes_to_break = random.choices(import_collector.import_nodes, k=num_bugs)
 
-        # Modify user's code.
-        modified_tree = tree.visit(ImportModifier(nodes_to_break))
+            # Modify user's code.
+            bugs_before = len(bugs_introduced)
+            modified_tree = tree.visit(ImportModifier(nodes_to_break, bugs_introduced))
+            if len(bugs_introduced) > bugs_before:
+                bug_made = True
 
-        # Rewrite user's code.
-        path.write_text(modified_tree.code)
+            # Rewrite user's code.
+            path.write_text(modified_tree.code)
 
-        print("  Modified file.")
+            print(f"  Modified file: {path.as_posix()}")
