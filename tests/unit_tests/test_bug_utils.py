7791415cbd8391b6d20b083b51b0d3486970bf6f
Eric Matthes <ehmatthes@gmail.com>
2025-03-31 08:27:15 -0400

    Simpler test.

--- tests/unit_tests/test_bug_utils.py
+++ tests/unit_tests/test_bug_utils.py
@@ -9,8 +9,6 @@ def test_remove_char():
     Take a short name. Call remove_char() 25 times. Should end up with all variations.
     """
     name = "part"
-    new_names = [bug_utils.remove_char(name) for _ in range(25)]
-    print(sorted(new_names))
+    new_names = set([bug_utils.remove_char(name) for _ in range(25)])
 
-    expected_names = ["art", "prt", "pat", "par"]
-    assert all(en in new_names for en in expected_names)
+    assert new_names == {"art", "prt", "pat", "par"}
