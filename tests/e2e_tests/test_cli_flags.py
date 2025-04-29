c267b00c5f3f2fed0bd8eaab0e8fa0a4cb99ab54
Eric Matthes <ehmatthes@gmail.com>
2025-04-29 09:35:33 -0400

    Failing test for new behavior.

--- tests/e2e_tests/test_cli_flags.py
+++ tests/e2e_tests/test_cli_flags.py
@@ -13,7 +13,25 @@ import sys
 
 
 def test_verbose_flag_true(tmp_path_factory, e2e_config):
-    """py-bugger --exception-type ModuleNotFoundError""" # --verbose"""
+    """py-bugger --exception-type ModuleNotFoundError --verbose"""
+
+    # Copy sample code to tmp dir.
+    tmp_path = tmp_path_factory.mktemp("sample_code")
+    print(f"\nCopying code to: {tmp_path.as_posix()}")
+
+    path_dst = tmp_path / e2e_config.path_name_picker.name
+    shutil.copyfile(e2e_config.path_name_picker, path_dst)
+
+    # Run py-bugger against directory.
+    cmd = f"py-bugger --exception-type ModuleNotFoundError --target-dir {tmp_path.as_posix()} --verbose"
+    cmd_parts = shlex.split(cmd)
+    stdout = subprocess.run(cmd_parts, capture_output=True).stdout.decode()
+
+    assert "All requested bugs inserted." in stdout
+    assert "name_picker.py" in stdout
+
+def test_verbose_flag_false(tmp_path_factory, e2e_config):
+    """py-bugger --exception-type ModuleNotFoundError"""
 
     # Copy sample code to tmp dir.
     tmp_path = tmp_path_factory.mktemp("sample_code")
@@ -28,4 +46,4 @@ def test_verbose_flag_true(tmp_path_factory, e2e_config):
     stdout = subprocess.run(cmd_parts, capture_output=True).stdout.decode()
 
     assert "All requested bugs inserted." in stdout
-    assert "name_picker.py" in stdout
+    assert "name_picker.py" not in stdout
