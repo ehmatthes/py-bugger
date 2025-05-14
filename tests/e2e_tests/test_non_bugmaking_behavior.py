918a89b2126aee76b2db5e220f995c9b82859fd1
Eric Matthes <ehmatthes@gmail.com>
2025-05-14 17:32:24 -0400

    Passing test for preserving file ending when no trailing newline present.

--- tests/e2e_tests/test_non_bugmaking_behavior.py
+++ tests/e2e_tests/test_non_bugmaking_behavior.py
@@ -30,5 +30,29 @@ def test_preserve_file_ending_trailing_newline(tmp_path_factory, e2e_config):
 
     # Check that last line is blank.
     lines = path_dst.read_text().splitlines()
-    breakpoint()
     assert lines[-1] == ""
+
+
+def test_preserve_file_ending_no_trailing_newline(tmp_path_factory, e2e_config):
+    """Test that trailing newlines are not introduced when not originally present."""
+
+    # Copy sample code to tmp dir.
+    tmp_path = tmp_path_factory.mktemp("sample_code")
+    print(f"\nCopying code to: {tmp_path.as_posix()}")
+
+    path_src = e2e_config.path_sample_scripts / "simple_indent_no_trailing_newline.py"
+    path_dst = tmp_path / path_src.name
+    shutil.copyfile(path_src, path_dst)
+
+    # Run py-bugger against file.
+    cmd = f"py-bugger --exception-type IndentationError --target-file {path_dst.as_posix()}"
+    print("cmd:", cmd)
+    cmd_parts = shlex.split(cmd)
+
+    stdout = subprocess.run(cmd_parts, capture_output=True).stdout.decode()
+
+    assert "All requested bugs inserted." in stdout
+
+    # Check that last line is not blank.
+    lines = path_dst.read_text().splitlines()
+    assert lines[-1] != ""
