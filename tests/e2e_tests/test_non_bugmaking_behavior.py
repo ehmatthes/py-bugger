204c3f7791f749b358666f4533b99610ca9fcb20
Eric Matthes <ehmatthes@gmail.com>
2025-05-15 12:52:55 -0400

    Use dog for testing file ending preservation.

--- tests/e2e_tests/test_non_bugmaking_behavior.py
+++ tests/e2e_tests/test_non_bugmaking_behavior.py
@@ -17,7 +17,7 @@ def test_preserve_file_ending_trailing_newline(tmp_path_factory, e2e_config):
     print(f"\nCopying code to: {tmp_path.as_posix()}")
 
     path_dst = tmp_path / e2e_config.path_simple_indent.name
-    shutil.copyfile(e2e_config.path_simple_indent, path_dst)
+    shutil.copyfile(e2e_config.path_dog, path_dst)
 
     # Run py-bugger against file.
     cmd = f"py-bugger --exception-type IndentationError --target-file {path_dst.as_posix()}"
@@ -40,7 +40,7 @@ def test_preserve_file_ending_no_trailing_newline(tmp_path_factory, e2e_config):
     tmp_path = tmp_path_factory.mktemp("sample_code")
     print(f"\nCopying code to: {tmp_path.as_posix()}")
 
-    path_src = e2e_config.path_sample_scripts / "simple_indent_no_trailing_newline.py"
+    path_src = e2e_config.path_sample_scripts / "dog_no_trailing_newline.py"
     path_dst = tmp_path / path_src.name
     shutil.copyfile(path_src, path_dst)
 
--- tests/sample_code/sample_scripts/simple_indent_no_trailing_newline.py
+++
@@ -1,2 +0,0 @@
-for num in [1, 2, 3]:
-    print(num)
