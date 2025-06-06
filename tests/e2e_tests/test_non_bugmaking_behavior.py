5b70dd3c62e1b10bc96d82e823485e265d144043
Eric Matthes <ehmatthes@gmail.com>
2025-06-06 15:12:35 -0400

    Test for appropriate handling of blank files.

--- tests/e2e_tests/test_non_bugmaking_behavior.py
+++ tests/e2e_tests/test_non_bugmaking_behavior.py
@@ -93,3 +93,27 @@ def test_preserve_file_ending_two_trailing_newline(tmp_path_factory, e2e_config,
     # Check that last line is not blank.
     lines = path_dst.read_text().splitlines(keepends=True)
     assert lines[-1] == "\n"
+
+@pytest.mark.parametrize("exception_type", ["IndentationError", "AttributeError", "ModuleNotFoundError"])
+def test_blank_file_behavior(tmp_path_factory, e2e_config, exception_type):
+    """Make sure py-bugger handles a blank file correctly."""
+    # Copy sample code to tmp dir.
+    tmp_path = tmp_path_factory.mktemp("sample_code")
+    print(f"\nCopying code to: {tmp_path.as_posix()}")
+
+    path_src = e2e_config.path_sample_scripts / "blank_file.py"
+    path_dst = tmp_path / path_src.name
+    shutil.copyfile(path_src, path_dst)
+
+    # Run py-bugger against file.
+    cmd = f"py-bugger --exception-type {exception_type} --target-file {path_dst.as_posix()}"
+    print("cmd:", cmd)
+    cmd_parts = shlex.split(cmd)
+
+    stdout = subprocess.run(cmd_parts, capture_output=True).stdout.decode()
+
+    assert "Unable to introduce any of the requested bugs." in stdout
+
+    # Check that file is still blank.
+    contents = path_dst.read_text()
+    assert contents == ""
