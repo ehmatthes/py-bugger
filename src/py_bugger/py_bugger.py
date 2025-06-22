89cf3f8dd08756572482ef303a0e0624c039fac2
Eric Matthes <ehmatthes@gmail.com>
2025-06-22 17:59:18 -0400

    IndentationError bugger moves -n handling to main file.

--- src/py_bugger/py_bugger.py
+++ src/py_bugger/py_bugger.py
@@ -39,8 +39,10 @@ def main():
             if buggers.attribute_error_bugger(py_files):
                 bugs_added += 1
         elif pb_config.exception_type == "IndentationError":
-            new_bugs_made = buggers.indentation_error_bugger(py_files)
-            bugs_added += new_bugs_made
+            # new_bugs_made = buggers.indentation_error_bugger(py_files)
+            # bugs_added += new_bugs_made
+            if buggers.indentation_error_bugger(py_files):
+                bugs_added += 1
 
     # Show a final success/fail message.
     msg = cli_messages.success_msg(bugs_added, pb_config.num_bugs)
