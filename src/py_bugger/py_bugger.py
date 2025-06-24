ddb678e7e254122eca343f31eaad58ebda5827da
Eric Matthes <ehmatthes@gmail.com>
2025-06-24 12:19:29 -0400

    No longer need to track number of bugs introduced.

--- src/py_bugger/py_bugger.py
+++ src/py_bugger/py_bugger.py
@@ -3,6 +3,7 @@ import random
 
 from py_bugger import buggers
 from py_bugger.utils import file_utils
+from py_bugger.utils.modification import modifications
 
 from py_bugger.cli.config import pb_config
 from py_bugger.cli.config import SUPPORTED_EXCEPTION_TYPES
@@ -18,28 +19,24 @@ def main():
     # Get a list of .py files we can consider modifying.
     py_files = file_utils.get_py_files(pb_config.target_dir, pb_config.target_file)
 
-    # Track how many bugs have been added.
-    bugs_added = 0
-
     # If --exception-type not specified, choose one.
     if not pb_config.exception_type:
         pb_config.exception_type = random.choice(SUPPORTED_EXCEPTION_TYPES)
 
-    # Currently, handles just one exception type per call.
+    # Currently, handles just one exception type per py-bugger call.
     # When multiple are supported, implement more complex logic for choosing which ones
     # to introduce, and tracking bugs. Also consider a more appropriate dispatch approach
     # as the project evolves.
     for _ in range(pb_config.num_bugs):
         if pb_config.exception_type == "ModuleNotFoundError":
-            if buggers.module_not_found_bugger(py_files):
-                bugs_added += 1
+            buggers.module_not_found_bugger(py_files)
         elif pb_config.exception_type == "AttributeError":
-            if buggers.attribute_error_bugger(py_files):
-                bugs_added += 1
+            buggers.attribute_error_bugger(py_files)
         elif pb_config.exception_type == "IndentationError":
-            if buggers.indentation_error_bugger(py_files):
-                bugs_added += 1
+            buggers.indentation_error_bugger(py_files)
 
     # Show a final success/fail message.
+    bugs_added = len(modifications)
     msg = cli_messages.success_msg(bugs_added, pb_config.num_bugs)
+    # msg = cli_messags.succes_msg()
     print(msg)
