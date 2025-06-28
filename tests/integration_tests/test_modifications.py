50f0dffe4003b46eaa36680ae136280f89b85659
Eric Matthes <ehmatthes@gmail.com>
2025-06-28 14:04:48 -0400

    Integration test for more bugs than can be introduced.

--- tests/integration_tests/test_modifications.py
+++ tests/integration_tests/test_modifications.py
@@ -61,3 +61,38 @@ def test_7_random_bugs(tmp_path_factory, test_config):
     # Make sure the correct kinds of exceptions were induced.
     exceptions_induced_str = [m.exception_induced.__name__ for m in modifications]
     assert sorted(exceptions_induced_str) == sorted(requested_bugs)
+
+
+def test_8_random_bugs(tmp_path_factory, test_config):
+    """Test equivalent of `py-bugger -n 8`.
+
+    Look for modifications that match bugs_requested.
+    Look for message that it can't add more bugs.
+    """
+    # Copy sample code to tmp dir.
+    tmp_path = tmp_path_factory.mktemp("sample_code")
+    print(f"\nCopying code to: {tmp_path.as_posix()}")
+
+    path_src = test_config.path_sample_scripts / "dog_bark.py"
+    path_dst = tmp_path / path_src.name
+    shutil.copyfile(path_src, path_dst)
+
+    # Make modifications against this directory.
+    # With the current random seed, 8 seems to be the smallest number of bugs
+    # where it can't finish adding bugs.
+    pb_config.target_file = path_dst
+    pb_config.num_bugs = 8
+    cli_utils.validate_config()
+
+    requested_bugs = py_bugger.main()
+
+    # Make sure one requested bug was unable to be generated.
+    assert len(modifications) == len(requested_bugs) - 1
+
+    # Make sure all requested bugs except one are in modifications.
+    exceptions_induced_str = [m.exception_induced.__name__ for m in modifications]
+    while exceptions_induced_str:
+        exception_induced = exceptions_induced_str.pop()
+        requested_bugs.remove(exception_induced)
+
+    assert len(requested_bugs) == 1
