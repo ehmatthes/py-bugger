88fe14a2be3c40db778244bc82fb7d87a89c2d48
Eric Matthes <ehmatthes@gmail.com>
2025-06-28 11:13:43 -0400

    Use test_config in both tests.

--- tests/integration_tests/test_modifications.py
+++ tests/integration_tests/test_modifications.py
@@ -8,19 +8,13 @@ from py_bugger.cli.config import pb_config
 from py_bugger.cli import cli_utils
 
 
-def test_modifications_modulenotfounderror(tmp_path_factory):
+def test_modifications_modulenotfounderror(tmp_path_factory,test_config):
     """Tests modifications after creating a ModuleNotFoundError."""
-    path_root = Path(__file__).parents[2]
-    path_tests = path_root / "tests"
-    path_reference_files = path_tests / "e2e_tests" / "reference_files"
-    path_sample_code = path_tests / "sample_code"
-    path_sample_scripts = path_sample_code / "sample_scripts"
-
     # Copy sample code to tmp dir.
     tmp_path = tmp_path_factory.mktemp("sample_code")
     print(f"\nCopying code to: {tmp_path.as_posix()}")
 
-    path_src = path_sample_scripts / "name_picker.py"
+    path_src = test_config.path_sample_scripts / "name_picker.py"
     path_dst = tmp_path / path_src.name
     shutil.copyfile(path_src, path_dst)
 
