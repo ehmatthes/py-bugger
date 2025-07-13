126de8ad36f15beca4cd2a6b6fcf171b67123fb2
Eric Matthes <ehmatthes@gmail.com>
2025-07-12 21:38:31 -0400

    Failing test that first try in a file with many tries is not always the one indented.

---
+++ tests/sample_code/sample_scripts/many_trys.py
@@ -0,0 +1,102 @@
+"""Simple file with many identical try-except blocks."""
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
+try:
+    5/0
+except ZeroDivisionError:
+    print("Can't divide by zero!")
+
