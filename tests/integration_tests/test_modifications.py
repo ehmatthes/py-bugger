51484920893f8467f473525c0c60ee8ab62b75c1
Eric Matthes <ehmatthes@gmail.com>
2025-07-14 08:50:34 -0400

    Test single line target for attr error and mnf error.

--- tests/integration_tests/test_modifications.py
+++ tests/integration_tests/test_modifications.py
@@ -303,6 +303,70 @@ def test_single_target_line_indentation_error(tmp_path_factory, test_config):
     requested_bugs = py_bugger.main()
 
     # Without including --target-lines, line 12 was modified. Make sure the line that
-    # was modified with --target-lines is in the target block.
+    # was modified with --target-lines is the target line.
+    assert len(modifications) == 1
+    assert modifications[0].line_num in pb_config.target_lines
+
+def test_single_target_line_attribute_error(tmp_path_factory, test_config):
+    """Test that the modified line is the targeted line.
+
+    This test was first written without --target-lines. Then a line
+    was identifed that didn't contain the bug that was originally made.
+    We're asserting that the change made is different than what would have been
+    introduced without this target line.
+    """
+    # Copy sample code to tmp dir.
+    tmp_path = tmp_path_factory.mktemp("sample_code")
+    print(f"\nCopying code to: {tmp_path.as_posix()}")
+
+    path_src = test_config.path_sample_scripts / "dog_bark.py"
+    path_dst = tmp_path / path_src.name
+    shutil.copyfile(path_src, path_dst)
+
+    # Make modifications against this file.
+    pb_config.target_file = path_dst
+    pb_config.exception_type = "AttributeError"
+    pb_config.target_lines = "14"
+    cli_utils.validate_config()
+
+    # Check that the --target-lines arg was converted correctly.
+    assert pb_config.target_lines == [14]
+
+    requested_bugs = py_bugger.main()
+
+    # Without including --target-lines, line 26 was modified. Make sure the line that
+    # was modified with --target-lines is the target line.
+    assert len(modifications) == 1
+    assert modifications[0].line_num in pb_config.target_lines
+
+def test_target_lines_block_modulenotfound_error(tmp_path_factory, test_config):
+    """Test that the modified line is the targeted line.
+
+    This test was first written without --target-lines. Then a line
+    was identifed that didn't contain the bug that was originally made.
+    We're asserting that the change made is different than what would have been
+    introduced without this target line.
+    """
+    # Copy sample code to tmp dir.
+    tmp_path = tmp_path_factory.mktemp("sample_code")
+    print(f"\nCopying code to: {tmp_path.as_posix()}")
+
+    path_src = test_config.path_sample_scripts / "ten_imports.py"
+    path_dst = tmp_path / path_src.name
+    shutil.copyfile(path_src, path_dst)
+
+    # Make modifications against this file.
+    pb_config.target_file = path_dst
+    pb_config.exception_type = "ModuleNotFoundError"
+    pb_config.target_lines = "4"
+    cli_utils.validate_config()
+
+    # Check that the --target-lines arg was converted correctly.
+    assert pb_config.target_lines == [4]
+
+    requested_bugs = py_bugger.main()
+
+    # Without including --target-lines, line 6 was modified. Make sure the line that
+    # was modified with --target-lines is the target line.
     assert len(modifications) == 1
     assert modifications[0].line_num in pb_config.target_lines
