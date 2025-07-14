98e89b223db86e33e0f15e2a7d533bb211f14369
Eric Matthes <ehmatthes@gmail.com>
2025-07-14 08:46:39 -0400

    Test single line target for IndentationError.

--- tests/integration_tests/test_modifications.py
+++ tests/integration_tests/test_modifications.py
@@ -177,6 +177,8 @@ def test_linenums_recorded(tmp_path_factory, test_config, exception_type):
     assert len(modifications) == 1
     assert modifications[0].line_num > 0
 
+# --- Tests for --target-lines ---
+
 def test_target_lines_block_indentation_error(tmp_path_factory, test_config):
     """Test that the modified line is in the targeted block.
 
@@ -269,6 +271,38 @@ def test_target_lines_block_modulenotfound_error(tmp_path_factory, test_config):
     requested_bugs = py_bugger.main()
 
     # Without including --target-lines, line 6 was modified. Make sure the line that
+    # was modified with --target-lines is in the target block.
+    assert len(modifications) == 1
+    assert modifications[0].line_num in pb_config.target_lines
+
+def test_single_target_line_indentation_error(tmp_path_factory, test_config):
+    """Test that the modified line is the targeted line.
+
+    This test was first written without --target-lines. Then a line
+    was identifed that didn't contain the bug that was originally made.
+    We're asserting that the change made is different than what would have been
+    introduced without this target line.
+    """
+    # Copy sample code to tmp dir.
+    tmp_path = tmp_path_factory.mktemp("sample_code")
+    print(f"\nCopying code to: {tmp_path.as_posix()}")
+
+    path_src = test_config.path_sample_scripts / "dog_bark.py"
+    path_dst = tmp_path / path_src.name
+    shutil.copyfile(path_src, path_dst)
+
+    # Make modifications against this file.
+    pb_config.target_file = path_dst
+    pb_config.exception_type = "IndentationError"
+    pb_config.target_lines = "16"
+    cli_utils.validate_config()
+
+    # Check that the --target-lines arg was converted correctly.
+    assert pb_config.target_lines == [16]
+
+    requested_bugs = py_bugger.main()
+
+    # Without including --target-lines, line 12 was modified. Make sure the line that
     # was modified with --target-lines is in the target block.
     assert len(modifications) == 1
     assert modifications[0].line_num in pb_config.target_lines
